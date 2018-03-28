9886e48708e5465ab34340ee79f982661a963ca7
David <dmstk97@gmail.com>
2018-03-27 20:53:16 -0600

    Reordenamiento del directorio
    El repo (CARLOS) consiste de 2 proyectos de NetBeans: Cliente y Servidor

---
+++ Servidor/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: stickman
+ *
+ * Created on March 27, 2018, 8:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
