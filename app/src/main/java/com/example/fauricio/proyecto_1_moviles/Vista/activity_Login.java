f22080c7369f4eee38d6350b120f287e4a91b0dd
fauricioNavarro <fauriciocr@gmail.com>
2018-03-28 18:02:48 -0600

    commit inicial

---
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/activity_Login.java
@@ -0,0 +1,15 @@
+package com.example.fauricio.proyecto_1_moviles.Vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fauricio.proyecto_1_moviles.R;
+
+public class activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__login);
+    }
+}
