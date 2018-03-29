c56b5908e17d2a333e50e489afc38d00c2788c43
fauricioNavarro <fauriciocr@gmail.com>
2018-03-28 21:11:40 -0600

    pantalla de login/registro

---
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/Registro.java
@@ -0,0 +1,15 @@
+package com.example.fauricio.proyecto_1_moviles.Vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fauricio.proyecto_1_moviles.R;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
