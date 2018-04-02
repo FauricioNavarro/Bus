579826d8a0d758519446b68915e43ab7fe3d10fb
Mauro <mauromm1996@gmail.com>
2018-04-02 00:05:38 -0600

    Primer intento Sign In con Google

--- app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/activity_Login.java
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/activity_Login.java
@@ -1,20 +1,41 @@
 package com.example.fauricio.proyecto_1_moviles.Vista;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fauricio.proyecto_1_moviles.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class activity_Login extends AppCompatActivity {
+public class activity_Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private TextView usuario,contraseña;
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
+    public static final int CODIGO_SIGN_IN = 777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        signInButton = findViewById(R.id.bt_SignInGoogle);
+
         usuario = findViewById(R.id.et_username);
         contraseña = findViewById(R.id.et_password);
     }
@@ -32,5 +53,39 @@ public class activity_Login extends AppCompatActivity {
     public void consulta_contraseña(View view){
         Intent intent = new Intent(getApplicationContext(),RecuperarPassword.class);
         startActivity(intent);
+    }
+
+    public void signIn(View view){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent, CODIGO_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CODIGO_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            goMainScreen();
+        }else{
+            Toast.makeText(this, R.string.fallo_sign_in, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, R.string.error_conexion, Toast.LENGTH_LONG).show();
     }
 }
