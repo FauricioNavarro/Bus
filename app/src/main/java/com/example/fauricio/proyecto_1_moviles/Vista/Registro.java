eec25c78e4ec1973d63f4874d32f00e95e519a40
fauricioNavarro <fauriciocr@gmail.com>
2018-04-07 18:59:32 -0600

    pantaña de añadir,eliminar y editar chofer

--- app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/Registro.java
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/Registro.java
@@ -1,7 +1,9 @@
 package com.example.fauricio.proyecto_1_moviles.Vista;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.example.fauricio.proyecto_1_moviles.R;
 
@@ -12,4 +14,5 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+
 }
