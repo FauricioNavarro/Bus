ae13ee613f2cc3b6f676a39d95b331d488335b4c
Unknown <fauriciocr@gmail.com>
2018-04-17 23:25:52 -0600

    tomela juan jo

--- app/src/main/java/com/example/fauricio/proyecto_1_moviles/Controlador/DAO_api.java
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Controlador/DAO_api.java
@@ -15,45 +15,42 @@ public class DAO_api extends AsyncTask<Void, Void, Void> {
 
     }
 
-    @Override
-    //Tiene que ser un do in background para que no caiga el sistema
     protected Void doInBackground(Void... params) {
 
         try {
 
-            //Codigo generado por postman
-		/*
-                final HttpResponse<String> response = Unirest.post("https://accounts.spotify.com/api/token")
-                        .header("authorization", "Basic NjRiMWRlZTljMzkzNGNhMzlhNzg0M2NlYWFkMjk2YmU6M2YwNTY2NWUwYjJhNGVkMzg4MjlhOTRhNzZjMmVkZjE=")
-                        .header("content-type", "application/x-www-form-urlencoded")
-                        .header("cache-control", "no-cache")
-                        .header("postman-token", "90a872a2-c311-a323-8afe-d61f53c195b9")
-                        .body("grant_type=client_credentials")
-                        .asString();
-
-
-            Log.i("===== AQUI  ======","ANTES");
             final HttpResponse<String> response = Unirest.get("https://bus-api-moviles.herokuapp.com/api/empresa/")
-                    .header("authorization", "c)+19fevc5zpvw-n#n!4434j)v--i$!_qo33=97!i^t57wed-2")
                     .header("content-type", "application/json")
                     .header("cache-control", "no-cache")
-                    .asString();*/
+                    .header("postman-token", "7abae682-b744-fc07-c986-06eeb5558724")
+                    //.body("{\n\t\"nombre\":\"MAMENDEZ COMPANY\",\n\t\"descripcion\":\"Compañia de buses\"\n}")
+                    .asString();
 
-            Log.i("===== AQUI ======","DESPUES");
-            final HttpResponse<String> response = Unirest.get("https://bus-api-moviles.herokuapp.com/api/empresa/").asString();
+
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+
                     String body = response.getBody().toString();
-                    Log.i("===== AQUI++ ======",body);
+                    //Integer last = body.indexOf("token_type") - 3;
+                    //String token = body.substring(17,last);
+                    Log.d("Token",body);
+                    //api.setAccessToken(token);
                 }
             });
+
+
         }  catch (UnirestException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private void runOnUiThread(Runnable runnable) {
+    private void runOnUiThread(Runnable token) {
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+
     }
 }
