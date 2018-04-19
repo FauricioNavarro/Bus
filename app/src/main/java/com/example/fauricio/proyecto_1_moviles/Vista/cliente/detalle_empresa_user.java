0fc90adb7afefbfebdf8691700ef520639dd406c
Unknown <fauriciocr@gmail.com>
2018-04-19 14:16:39 -0600

    Pantalla de admin y usuario casi listas, empezando la pantalla chofer

---
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Vista/cliente/detalle_empresa_user.java
@@ -0,0 +1,13 @@
+package com.example.fauricio.proyecto_1_moviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detalle_empresa_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_empresa_user);
+    }
+}
