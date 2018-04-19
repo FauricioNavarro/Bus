44e4081af46b31b0afbc9cec3264721292b49683
Juan Jose A. Castro <juan_jop96@hotmail.com>
2018-04-18 22:23:50 -0600

    Auto stash before merge of "master" and "origin/master"
    DAO Completo solicita 4 para registrar o Googlogin y 2 para login Simple

--- app/src/main/java/com/example/fauricio/proyecto_1_moviles/Controlador/DAO_api.java
+++ app/src/main/java/com/example/fauricio/proyecto_1_moviles/Controlador/DAO_api.java
@@ -1,6 +1,7 @@
 package com.example.fauricio.proyecto_1_moviles.Controlador;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
@@ -17,6 +18,7 @@ public class DAO_api extends AsyncTask<String, Void, String> {
 
     }
 
+
     protected String doInBackground(String... params) {
         String tipoEjecucion = params[0];
         switch (tipoEjecucion){
@@ -29,6 +31,8 @@ public class DAO_api extends AsyncTask<String, Void, String> {
                     try {
                         student1.put("email", params[1]);
                         student1.put("password", params[2]);
+                        student1.put("first_name", params[3]);
+                        student1.put("last_name",params[4]);
                      } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -47,6 +51,7 @@ public class DAO_api extends AsyncTask<String, Void, String> {
                 try {
                     JSONObject login = new JSONObject();
                     try {
+
                         login.put("email", params[1]);
                         login.put("password", params[2]);
                     } catch (JSONException e) {
@@ -76,8 +81,101 @@ public class DAO_api extends AsyncTask<String, Void, String> {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     } catch (UnirestException e) {
-                    e.printStackTrace();
+                        e.printStackTrace();
                 }
+
+            }break;
+            case "GOOGLogin":{
+                try {
+                        JSONObject login = new JSONObject();
+                        try {
+
+                            login.put("email", params[1]);
+                            login.put("password", params[2]);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        HttpResponse<String> response = Unirest.post("https://bus-api-moviles.herokuapp.com/api/user/login/")
+                                .header("content-type", "application/json")
+                                .header("cache-control", "no-cache")
+                                .header("postman-token", "9285d672-f19a-0f5d-92d4-b6de9bf02303")
+                                .body(String.valueOf(login))
+                                .asString();
+                        JSONObject json1 = new JSONObject(response.getBody().toString());
+                    if (json1.get("token") != null) {
+                        String Token = (String) json1.get("token");
+                        HttpResponse<String> response1 = Unirest.get("https://bus-api-moviles.herokuapp.com/api/user/view")
+                                .header("content-type", "application/json")
+                                .header("authorization", "JWT " + Token)
+                                .header("cache-control", "no-cache")
+                                .asString();
+                        JSONObject json2 = new JSONObject(response1.getBody().toString());
+                        //GUARDAR ESTE ID EN SHARED PREFFERENCES
+                        int ID = json2.getInt("id");
+                        HttpResponse<String> finalresponse = Unirest.get("https://bus-api-moviles.herokuapp.com/api/users/" + String.valueOf(ID) + "/")
+                                .header("cache-control", "no-cache")
+                                .header("postman-token", "a9e5810d-45c1-4089-3bc0-c77e699d58dc")
+                                .asString();
+                        Log.d("GOOGLogin","Login con GOOGLE");
+                        return finalresponse.getBody().toString();
+
+                        }
+                    } catch (JSONException e) {
+
+                    JSONObject sigup = new JSONObject();
+                    JSONObject login = new JSONObject();
+
+                    try {
+                        login.put("email", params[1]);
+                        login.put("password", params[2]);
+                        sigup.put("email", params[1]);
+                        sigup.put("password", params[2]);
+                        sigup.put("first_name", params[3]);
+                        sigup.put("last_name",params[4]);
+                    } catch (JSONException e1) {
+                        e1.printStackTrace();
+                    }
+                    try {
+                        HttpResponse<String> response = Unirest.post("https://bus-api-moviles.herokuapp.com/api/user/create/")
+                                .header("content-type", "application/json")
+                                .header("cache-control", "no-cache")
+                                .header("postman-token", "4f554794-9859-39ec-a223-b2b5cbc57610")
+                                .body(String.valueOf(sigup))
+                                .asString();
+
+                        HttpResponse<String> response1 = Unirest.post("https://bus-api-moviles.herokuapp.com/api/user/login/")
+                                .header("content-type", "application/json")
+                                .header("cache-control", "no-cache")
+                                .header("postman-token", "9285d672-f19a-0f5d-92d4-b6de9bf02303")
+                                .body(String.valueOf(login))
+                                .asString();
+                        JSONObject json1 = new JSONObject(response1.getBody().toString());
+                        String Token = (String) json1.get("token");
+                        HttpResponse<String> response2 = Unirest.get("https://bus-api-moviles.herokuapp.com/api/user/view")
+                                .header("content-type", "application/json")
+                                .header("authorization", "JWT "+Token)
+                                .header("cache-control", "no-cache")
+                                .asString();
+                        JSONObject json2 = new JSONObject(response2.getBody().toString());
+                        //GUARDAR ESTE ID EN SHARED PREFFERENCES
+                        int ID = json2.getInt("id");
+                        HttpResponse<String> finalresponse = Unirest.get("https://bus-api-moviles.herokuapp.com/api/users/"+String.valueOf(ID)+"/")
+                                .header("cache-control", "no-cache")
+                                .header("postman-token", "a9e5810d-45c1-4089-3bc0-c77e699d58dc")
+                                .asString();
+                        Log.d("GOOGLogin","Usuario Nuevo de GOOGLE");
+                        return finalresponse.getBody().toString();
+
+                    } catch (UnirestException e1) {
+                        e1.printStackTrace();
+                    } catch (JSONException e1) {
+                        e1.printStackTrace();
+                    }
+
+                    e.printStackTrace();
+                    } catch (UnirestException e) {
+                        e.printStackTrace();
+                    }
 
             }break;
         }
